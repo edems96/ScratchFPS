2b808cc82379dc6091eadb394617f0a5a45eea76
Adam Torok <edems96@gmail.com>
2015-11-12 21:38:50 +0100

    RGB

---
+++ src/RGB.cpp
@@ -0,0 +1,13 @@
+#include "RGB.h"
+
+RGB::RGB() {
+	this->r = 0;
+	this->g = 0;
+	this->b = 0;
+}
+
+RGB::RGB(unsigned char r, unsigned char g, unsigned b) {
+	this->r = r;
+	this->g = g;
+	this->b = b;
+}
---
+++ src/RGB.h
@@ -0,0 +1,12 @@
+#ifndef RGB_H
+#define RGB_H
+
+class RGB {
+	
+	public:
+		unsigned char r, g, b;
+		
+		RGB();
+		RGB(unsigned char r, unsigned char g, unsigned b);
+		
+}
